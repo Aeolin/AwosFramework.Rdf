124d7778d4b04fc8c4a1ca0236eccce38b1f448b
Aeolin Ferjünnoz <aeolin@aeol.in>
2024-02-12 09:46:48 +0100

    -added lib

---
+++ AwosFramework.Rdf.Lib/Core/Iri.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwosFramework.RDF.Lib.Core
+{
+	public class Iri
+	{
+		public Uri Value { get; private set; }
+
+		public Iri(string value)
+		{
+			Value = new Uri(value);
+		}
+
+		public override string ToString()
+		{
+			return $"<{Value}>";
+		}
+
+		public static implicit operator Iri(string value)
+		{
+			return new Iri(value);
+		}
+		
+		public static implicit operator Uri(Iri iri) => iri.Value;
+		public static implicit operator string(Iri iri) => iri.Value.ToString();
+		public static bool operator ==(Iri a, Iri b) => a.Value == b.Value;
+		public static bool operator !=(Iri a, Iri b) => a.Value != b.Value;
+
+
+		public override bool Equals(object obj)
+		{
+			if (obj is Iri)
+			{
+				return Value == ((Iri)obj).Value;
+			}
+			return false;
+		}
+		public override int GetHashCode()
+		{
+			return Value.GetHashCode();
+		}
+	}
+}
